d2231ab89c6179fd3d61a2e94f510e8414244775
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-07-15 17:58:20 +0800

    auth layout - wip

--- app/backoffice-dashboard/auth/layout.tsx
+++ app/backoffice-dashboard/auth/layout.tsx
@@ -1,3 +1,22 @@
+import { Flex } from "@radix-ui/themes";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="h-full w-full">{children}</div>;
+  return (
+    <Flex className="h-screen w-full" justify="center" align="center">
+      <Flex
+        direction="column"
+        gap="4"
+        justify="center"
+        className="h-screen min-w-[400px]"
+      >
+        <img
+          src="/assets/brand-logo.png"
+          alt="DentalEase"
+          width={100}
+          height={100}
+        />
+        {children}
+      </Flex>
+    </Flex>
+  );
 }
