9872fa07f8bd90e270707ad58e206819723b2b34
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-07-31 09:06:45 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import { Flex } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/backoffice-dashboard/auth/signin");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <Flex
       className="h-screen w-screen bg-slate-900"
