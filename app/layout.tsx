46fdf8cc5e1522578ec815688905e68f10a7e39e
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-07-31 14:57:22 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { HeaderNav, FooterNav } from "@/components";
 import "./app.css";
 
 const inter = Inter({
@@ -18,7 +19,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Theme scaling="90%">
-          {children}
+          <div className="min-h-screen">
+            <HeaderNav />
+            <main className="pt-16">{children}</main>
+            <FooterNav />
+          </div>
           <ToastContainer
             position="top-center"
             autoClose={3000}
